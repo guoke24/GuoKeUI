2fcf9c89d65cbc71f5826250a4eac35891b3f043
guohao <guoke24@qq.com>
2021-03-03 20:06:17 +0800

    默认弹弹球

--- guokeui/src/main/java/com/guohao/guokeui/TestFlingBallActivity.kt
+++ guokeui/src/main/java/com/guohao/guokeui/TestFlingBallActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class TestFlingBallActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        title = "弹弹球"
         setContentView(R.layout.activity_test_fling_ball)
     }
 }
